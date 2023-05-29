31e29def430a06c7ea9eb4fab5abb35b06079bc2
Lakshmi-Thirumala <klthirumala18@gmail.com>
2023-05-29 16:52:02 +0530

    Adding Main code

---
+++ src/com/ArrayList/ArrayListExample.java
@@ -0,0 +1,18 @@
+package com.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		List<String> fruits = new ArrayList<>();
+		fruits.add("Banana");
+		fruits.add("Apple");
+		fruits.add("Mango");
+		fruits.add("Orange");
+		
+		System.out.println(fruits);
+	}
+
+}
